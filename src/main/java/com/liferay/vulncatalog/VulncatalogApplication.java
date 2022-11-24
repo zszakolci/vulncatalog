78c3b79a2b3d42694a66111f89829c88d4f5192c
Istvan Sajtos <istvan.sajtos@liferay.com>
2022-11-24 01:37:25 +0100

    LRSUHU-2914 Initial commit for backend

---
+++ src/main/java/com/liferay/vulncatalog/VulncatalogApplication.java
@@ -0,0 +1,13 @@
+package com.liferay.vulncatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VulncatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VulncatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liferay/vulncatalog/VulncatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liferay.vulncatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VulncatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
