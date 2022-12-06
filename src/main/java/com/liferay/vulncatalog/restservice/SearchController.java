a3248c922330268ed6b46f4c17184400debd3242
Istvan Sajtos <istvan.sajtos@liferay.com>
2022-12-06 11:21:43 +0100

    LRSUHU-2924 Add rest contoller, initially serving dummy data

---
+++ src/main/java/com/liferay/vulncatalog/restservice/SearchController.java
@@ -0,0 +1,45 @@
+package com.liferay.vulncatalog.restservice;
+
+import com.liferay.vulncatalog.persistence.entity.Ticket;
+import com.liferay.vulncatalog.persistence.entity.Vulnerability;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+public class SearchController {
+
+	@GetMapping("/search")
+	public List<Object> search(
+		@RequestParam(value = "input", defaultValue = "") String input) {
+
+		List<Vulnerability> vulnerabilities = new LinkedList();
+
+		vulnerabilities.add(new Vulnerability("CVE-2021-33813",
+			"https://nvd.nist.gov/vuln/detail/CVE-2021-33813", ""));
+		vulnerabilities.add(new Vulnerability("CVE-2021-30468",
+			"https://nvd.nist.gov/vuln/detail/CVE-2021-30468", ""));
+
+		List<Ticket> tickets = new LinkedList();
+
+		Ticket ticket1 = new Ticket("LPS-00001", "LPE-0001", "CVE-2021-33813", 1, "7.2.10", "7.2.x");
+		ticket1.setId(1);
+		tickets.add(ticket1);
+		Ticket ticket2 = new Ticket("LPS-00002", "LPE-0002", "CVE-2021-30468", 2, "7.3.10", "7.3.x");
+		ticket2.setId(2);
+		tickets.add(ticket2);
+
+		List<Object> result = new LinkedList();
+
+		result.add(vulnerabilities);
+		result.add(tickets);
+
+		return result;
+	}
+}
