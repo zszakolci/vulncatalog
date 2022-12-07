3e912fa8cd459c58c48c85a5eb5fef47868e7a06
Szakolci Zalán <zalan.szakolci@liferay.com>
2022-12-07 07:49:26 +0100

    LRSUHU-2925 Added links

--- frontend/src/Components/SearchList.js
+++ frontend/src/Components/SearchList.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import Vulnentry from './Vulnentry';
+import '../styles/VulnResults.scss';
 //import TicketRef from './TicketRef';
 
 function SearchList({ filteredCatalog }) {
   const vulnEntries = filteredCatalog.map(vulnentry =>  <Vulnentry key={vulnentry.id} vulnentry={vulnentry} />); 
-  //const ticketRefs = filteredCatalog[1].map(ticketRef =>  <TicketRef key={ticketRef.id} ticketRef={ticketRef} />);
  console.log(vulnEntries);
   return (
-    <div>
-    <table>
-        <tbody>
+    <div className='vuln-container'>
+        <h2>Vulnerability Description</h2>
+    <ul className='vuln-table'>
+        <li className='vuln-table-header'>
+            <div className='vuln col-1'>CVE Code</div>
+            <div className='vuln col-2'>URL</div>
+            <div className='vuln col-3'>Description</div>
+        </li>
         {vulnEntries}
-      </tbody>
-    </table>
+    
+    </ul>
     </div>
     
   );
--- frontend/src/Components/TicketList.js
+++ frontend/src/Components/TicketList.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import TicketRef from './TicketRef';
+import '../styles/ticketResults.scss'
 
 function TicketList({ filteredCatalog }) {
  
   const ticketRefs = filteredCatalog.map(ticketRef =>  <TicketRef key={ticketRef.id} ticketRef={ticketRef} />);
   console.log(ticketRefs);
   return (
-    <div>
-    <table>
-        <tbody>
+    <div className='ticket-container'>
+        <h2>Related Tickets</h2>
+    <ul className='ticket-table'>
+        <li className='ticket-table-header'>
+            <div className='ticket col-1'>LPE</div>
+            <div className='ticket col-2'>LPS</div>
+            <div className='ticket col-3'>CVE</div>
+            <div className='ticket col-4'>Library</div>
+            <div className='ticket col-5'>Affects Version</div>
+            <div className='ticket col-6'>Fixed Version</div>
+        </li>
          {ticketRefs}
-      </tbody>
-    </table>
+      </ul>
     </div>
     
   );
