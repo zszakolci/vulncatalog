bcca3845ec4d776fff26e6c6af4a41d2065afbe3
Szakolci Zalán <zalan.szakolci@liferay.com>
2023-01-25 08:48:14 +0100

    testing with react-hook-form, changing swr to native fetch in submit

--- frontend/app/components/addVulnForm.js
+++ frontend/app/components/addVulnForm.js
@@ -1,32 +1,38 @@
 "use client";
 
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Button } from '@mui/material';
 import styles from './addVulnForm.module.css'
-import useSWR from 'swr'
-import { useForm,useController } from "react-hook-form";
 
+import { useForm,Controller } from "react-hook-form";
+import { usePromiseTracker, trackPromise} from "react-promise-tracker";
 
 
 function AddVulnForm(){
     const [urlFieldValue, setUrlFieldValue] = React.useState('');
-    const CVEInput = React.createRef();
-    const descriptionInput = React.createRef();
-    const formRef = React.useRef(null);
-     const fetcher = (...args) => fetch(...args).then(res => res.json());
-     const [post,setPost] = useState(false);
-    const cveInputName="cve";
-    const restURL = (CVEInput.current  && descriptionInput.current) ? `http://localhost:8080/vulnerability/add?id=${CVEInput.current.value}&url=${urlFieldValue}&description=${descriptionInput.current.value}`: "";
-    const {data,error,isLoading} = useSWR( post ? restURL: null,fetcher); 
-    const { register, handleSubmit,formState: { errors }, control } = useForm({
+    const CVEInput = useRef(null);
+    const descriptionInput = useRef(null);
+    const formRef = useRef(null);
+    //const cveInputName="cve";
+    const [errorMessage, setErrorMessage] = useState("");
+    const [submitError,setSubmitError] = useState(false);
+    const { loading } = usePromiseTracker();
+    //const restURL = (CVEInput.current  && descriptionInput.current) ? `http://localhost:8080/vulnerability/add?id=${CVEInput.current.value}&url=${urlFieldValue}&description=${descriptionInput.current.value}`: "";
+
+    //console.log(restURL);
+    //const {data,error,isLoading} = useSWR( post ? restURL: null,fetcher); 
+
+     const { register, handleSubmit, watch, formState: { errors }, control } = useForm({
         mode: "onTouched",
         defaultValues: {
-            cveInputName: '',
+            cve: '',
             url: ''
           }
-    });
+    });  
 
-   
+    register("cve", {required: true});
+    console.log("errors",errors);
+    const cve = watch("cve");
     const constructURL = (cve) =>
     {
         return "https://nvd.nist.gov/vuln/detail/" + cve;
@@ -41,31 +47,86 @@ function AddVulnForm(){
 
 const handleURLChange = (e) =>
 {
+
     setUrlFieldValue(e.target.value);
 }
 
-const handleFormSubmit = async (event) =>
+const handleFormSubmit =  (event) =>
 {
-    console.log("in handleSubmit");
-    setPost(true);
+    const restURL = `http://localhost:8080/vulnerability/add?id=${CVEInput.current.value}&url=${urlFieldValue}&description=${descriptionInput.current.value}`;
+    console.log("in handlesubmit");
+    trackPromise(
+    fetch(restURL)
+        .then(async response => {
+            //const data = await response.json();
+
+            // check for error response
+            if (!response.ok) {
+                // get error message from body or default to response statusText
+                const error = (data && data.message) || response.statusText;
+                return Promise.reject(error);
+            }
+            setSubmitError(false);
+            CVEInput.current.value = "";
+            descriptionInput.current.value = "";
+            setUrlFieldValue("");
+            //this.setState({ totalReactPackages: data.total })
+        })
+        .catch(error => {
+            setErrorMessage(error.toString());
+            setSubmitError(true);
+            console.error('There was an error!', error);
+        }));
+    //setPost(true);
     event.preventDefault;
-    formRef.current.submit();
+    //formRef.current.submit();
    
     
 }
     return (
         <div className={"box"} >
             <form ref={formRef} className={styles.addVulnForm } >
-
-                    <div  className={styles.listItem}><input name={cveInputName}{...register("id",{ required: true })} onBlur={handleCVEInputBlur}
-       className="inputField" type="text" placeholder="CVE ID" /></div>
-                     {errors.id && <div className="errorMessage">This field is required</div>} 
+            
+            <Controller
+                 render={({
+                    field: { onBlur, value, name={cve}, ref, placeholder="CVE ID", className="inputField" },
+                    fieldState: { invalid, isTouched, isDirty, error },
+                  }) => (
+                    <div  className={styles.listItem}>
+                    <input
+                      value={value}
+                      required
+                      onBlur={handleCVEInputBlur} // notify when input is touched
+                      ref={CVEInput} // wire up the input ref
+                      placeholder={placeholder}
+                      name={name}
+                      className={className}
+                    />
+                    {console.log(invalid)}
+                    {console.log(error)}
+                    {invalid && isTouched && <div className="error-message">szar</div>}
+                    </div> 
+                  )}
+                control={control}
+                rules={{ required: true }}
+                name={cve}
+                
+                
+            />
+            {cve}
+            {errors.cve  && 
+  <div className="error-message">This field is required</div>}
+                       
+                    {/* <div  className={styles.listItem}><input ref={CVEInput} name={cveInputName} onBlur={handleCVEInputBlur}
+       className="inputField" type="text" placeholder="CVE ID" /></div> */}
+                     {/* {errors.id && <div className="errorMessage">This field is required</div>}  */}
                     <div className={styles.URLContainer}><input className={"inputField " + styles.URLItem} type="url" placeholder="URL" onChange={handleURLChange} value={urlFieldValue} /> </div>
                     {/* {errors.url && <div className="errorMessage">This field is required</div>} */}
                     <div className={styles.listItem}><textarea ref={descriptionInput} className='textArea' placeholder="DESCRIPTION"/></div>
-                <Button onClick={handleSubmit(handleFormSubmit)} className={styles.addButton} variant="contained">
+                <Button onClick={handleFormSubmit} className={styles.addButton} variant="contained">
                     Add
-                </Button > {error ? <label className="errorMessage">Error:</label> : ''}
+                </Button > {submitError && <label className="errorMessage">Error: {errorMessage}</label> }
+                {loading && <label className="errorMessage">Loading...</label> }
             </form>
         </div>
       );
