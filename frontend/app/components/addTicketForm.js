973d63fefa4d8e0354ea7503ce0cd1fb93704c8c
Szakolci Zalán <zalan.szakolci@liferay.com>
2023-01-25 16:19:54 +0100

    adjustmant with library param

--- frontend/app/components/addTicketForm.js
+++ frontend/app/components/addTicketForm.js
@@ -4,8 +4,11 @@ import CVESearch from './cveSearch'
 import LibrarySearch from './librarySearch';
 import VersionSearch from './versionSearch';
 import React, { useState } from 'react';
-import useSWR from 'swr';
+import { usePromiseTracker, trackPromise} from "react-promise-tracker";
+
 export const ErrorContext = React.createContext();
+
+
 function AddTicketForm(){
     const CVEIDInput = React.createRef();
     const LPEInput = React.createRef();
@@ -14,21 +17,55 @@ function AddTicketForm(){
     const libraryInput = React.createRef();
     const affectedInput = React.createRef();
     const fixedInput = React.createRef();
-    const fetcher = (...args) => fetch(...args).then(res => res.json());
-    const [post,setPost] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+    const [submitError,setSubmitError] = useState(false);
+    //const fetcher = (...args) => fetch(...args).then(res => res.json());
+    //const [post,setPost] = useState(false);
     const [err, setErr] = useState(null);
-    const restURL = 
-    CVEIDInput.current && LPEInput.current && LSVInput.current && LPSInput.current
-    && libraryInput.current && affectedInput.current && fixedInput.current
-    ?`http://localhost:8080/ticket/add?vulnerabilityId=${CVEInput.current.value}&lpeId=${LPEInput.current.value}&lpsId=${LPSInput.current.value}&lsvId=${LSVInput.current.value}&libraryId=${libraryInput.current.value}&affectedversion=${affectedInput.current.value}&fixedversion=${fixedInput.current.value}`
-    : '';
+   
 
     //const {data,error,isLoading} = useSWR( post ? restURL: null); 
    
     const handleSubmit = async (event) =>
     {
-        setPost(true);
-        event.preventDefault();  
+        const restURL = 
+        CVEIDInput.current && LPEInput.current && LSVInput.current && LPSInput.current
+        && libraryInput.current && affectedInput.current && fixedInput.current
+        ?`http://localhost:8080/ticket/add?vulnerabilityId=${CVEIDInput.current.value}&lpeId=${LPEInput.current.value}&ticketId=${LPSInput.current.value}&lsvId=${LSVInput.current.value}&library=${libraryInput.current.value}&affectedversion=${affectedInput.current.value}&fixedversion=${fixedInput.current.value}`
+        : '';
+        event.preventDefault(); 
+        console.log("in handlesubmit");
+        console.log(restURL);
+        trackPromise(
+        fetch(restURL)
+            .then(async response => {
+                //const data = await response.json();
+    
+                // check for error response
+                if (!response.ok) {
+                    // get error message from body or default to response statusText
+                    const error = (data && data.message) || response.statusText;
+                    return Promise.reject(error);
+                }
+                setSubmitError(false);
+                CVEIDInput.current.value = "";
+                LPEInput.current.value = "";
+                LSVInput.current.value = "";
+                LPSInput.current.value = "";
+                libraryInput.current.value = "";
+                affectedInput.current.value = "";
+                fixedInput.current.value = "";
+                //this.setState({ totalReactPackages: data.total })
+            })
+            .catch(error => {
+                setErrorMessage(error.toString());
+                setSubmitError(true);
+                console.error('There was an error!', error);
+            }));
+        //setPost(true);
+        event.preventDefault;
+        //formRef.current.submit();
+         
     }
 
     return (
@@ -40,7 +77,7 @@ function AddTicketForm(){
                     <div className={styles.listItem}><input ref={LPEInput} className="inputField" type="text" placeholder="LPE" /></div>
                     <div className={styles.listItem}><input ref={LSVInput} className="inputField" type="text" placeholder="LSV" /></div>
                     <div className={styles.listItem}><input ref={LPSInput} className="inputField" type="text" placeholder="LPS" /></div>
-                    <LibrarySearch ref={libraryInput}/>
+                    <input ref={libraryInput} className="inputField" type="text" placeholder="Affected library"/>
                     <VersionSearch ref={affectedInput} label="Affects Version"/>
                     <VersionSearch ref={fixedInput} label="Fixed Version"/>
                     
