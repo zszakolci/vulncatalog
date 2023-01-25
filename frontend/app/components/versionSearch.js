b9428684bf15e34c817f22605d14c04af70f6287
Szakolci Zalán <zalan.szakolci@liferay.com>
2023-01-25 16:19:54 +0100

    adjustmant with library param

--- frontend/app/components/versionSearch.js
+++ frontend/app/components/versionSearch.js
@@ -10,13 +10,12 @@ const VersionSearch = ({label},ref) =>{
     const fetcher = (...args) => fetch(...args).then(res => res.json());
     const {data,error,isLoading} = useSWR( 'http://localhost:8080/version/get-all',fetcher ); 
     const returned = data ? data : [];
-    console.log(error);
     const versions = error ? [error.toString()] : returned;
    // let counter = 1;
     //const versionOpts = versions.map(version => ({label:version, id:counter++}));
 
     return(
-        <div className={styles.comboBoxContainer}> <Autocomplete options={versions} disablePortal renderInput={(params) => <TextField {...params} className="combobox" label={label} placeholder={label} ref={ref} />} /> </div>
+        <div className={styles.comboBoxContainer}> <Autocomplete options={versions} disablePortal renderInput={(params) => <TextField {...params} className="combobox" label={label} placeholder={label} inputRef={ref} />} /> </div>
         
      );
 }
