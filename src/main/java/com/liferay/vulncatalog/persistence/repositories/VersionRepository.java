3019a1bb4c5948fa81291c6bca2b5a155ec11256
Istvan Sajtos <istvan.sajtos@liferay.com>
2023-03-08 12:07:18 +0100

    LRSUHU-2970 Fix design flaw
    Currently we use foreign key attributes in ticket table to represent connections to vulnerabilities and versions.
    This result in redundant data storage.
    The new design uses join tables to represent the connections, so the data in base tables remain clean and easily maintainable.

--- src/main/java/com/liferay/vulncatalog/persistence/repositories/VersionRepository.java
+++ src/main/java/com/liferay/vulncatalog/persistence/repositories/VersionRepository.java
@@ -3,6 +3,7 @@ package com.liferay.vulncatalog.persistence.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.liferay.vulncatalog.persistence.entity.Version;
+import com.liferay.vulncatalog.persistence.entity.Vulnerability;
 
 import java.util.List;
 
@@ -10,4 +11,5 @@ public interface VersionRepository extends JpaRepository<Version, Long> {
 
 	List<Version> findByIdContainingIgnoreCase(String id);
 
+	List<Version> findByIdIn(List<String> ids);
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,15 +10,40 @@ INSERT INTO vulnerability (id, url, description) VALUES
 ('CVE-2021-30468', 'https://nvd.nist.gov/vuln/detail/CVE-2021-30468', ''),
 ('CVE-2021-33813', 'https://nvd.nist.gov/vuln/detail/CVE-2021-33813', '');
 
-INSERT INTO ticket (id, ticketId, lpeId, lsvId, vulnerabilityId, library, affectedVersion, fixedVersion) VALUES
-(1, 'LPS-135892', 'LPE-17346', 'LSV-908', 'CVE-2021-30468', null, '7.3.X', '7.3.X'),
-(2, 'LPS-135892', 'LPE-17346', 'LSV-908', 'CVE-2021-30468', null, 'master', 'master'),
-(3, 'LPS-166489', 'LPE-17670', null, 'CVE-2021-30468', null, '7.2.X', '7.2.X'),
-(4, 'LPS-166489', 'LPE-17670', null, 'CVE-2021-30468', null, '7.1.X', '7.1.X'),
-(5, 'LPS-166489', 'LPE-17670', null, 'CVE-2021-30468', null, '7.0.X', null),
-(6, 'LPS-166489', 'LPE-17670', null, 'CVE-2021-30468', null, '6.2.X EE', null),
-(7, 'LPS-137136', 'LPE-17362', 'LSV-923', 'CVE-2021-33813', null, 'master', 'master'),
-(8, 'LPS-168194', 'LPE-17669', null, 'CVE-2021-33813', null, '7.3.X', '7.3.X'),
-(9, 'LPS-168194', 'LPE-17669', null, 'CVE-2021-33813', null, '7.2.X', '7.2.X'),
-(10, 'LPS-168194', 'LPE-17669', null, 'CVE-2021-33813', null, '7.1.X', '7.1.X'),
-(11, 'LPS-168194', 'LPE-17669', null, 'CVE-2021-33813', null, '7.0.X', null);
+INSERT INTO ticket (id, lpeId, lsvId, library) VALUES
+('LPS-135892', 'LPE-17346', 'LSV-908', null),
+('LPS-166489', 'LPE-17670', null, null),
+('LPS-137136', 'LPE-17362', 'LSV-923', null),
+('LPS-168194', 'LPE-17669', null, null);
+
+INSERT INTO ticket_vulnerability (ticketId, vulnerabilityId) VALUES
+('LPS-135892', 'CVE-2021-30468'),
+('LPS-166489', 'CVE-2021-30468'),
+('LPS-137136', 'CVE-2021-33813'),
+('LPS-168194', 'CVE-2021-33813');
+
+INSERT INTO ticket_affectedversion (ticketId, affectedVersion) VALUES
+('LPS-135892', '7.3.X'),
+('LPS-135892', 'master'),
+('LPS-166489', '7.2.X'),
+('LPS-166489', '7.1.X'),
+('LPS-166489', '7.0.X'),
+('LPS-166489', '6.2.X EE'),
+('LPS-137136', 'master'),
+('LPS-168194', '7.3.X'),
+('LPS-168194', '7.2.X'),
+('LPS-168194', '7.1.X'),
+('LPS-168194', '7.0.X');
+
+INSERT INTO ticket_fixedversion (ticketId, fixedVersion) VALUES
+('LPS-135892', '7.3.X'),
+('LPS-135892', 'master'),
+('LPS-166489', '7.2.X'),
+('LPS-166489', '7.1.X'),
+('LPS-166489', '7.0.X'),
+('LPS-166489', '6.2.X EE'),
+('LPS-137136', 'master'),
+('LPS-168194', '7.3.X'),
+('LPS-168194', '7.2.X'),
+('LPS-168194', '7.1.X'),
+('LPS-168194', '7.0.X');
